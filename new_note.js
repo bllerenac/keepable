c546e4836ebe4315c35de45be045b858453b5138
bllerenac <bllerenac@uni.pe>
2021-01-28 16:37:55 -0500

    add function read and wrinte notes

--- new_note.js
+++ new_note.js
@@ -1,28 +1,63 @@
-const global =  {
+const GLOBAL =  {
   notes: [
     {
-      tittle: "tittle 01",
+      title: "tittle 01",
       content: "content 01",
       color: "red",
-      date: new Date.today
+      trash: false,
+      date: new Date('2020-10-12')
     },
     {
-      tittle: "tittle 01",
+      title: "tittle 01",
       content: "content 01",
       color: "red",
-      date: new Date.today
+      trash: false,
+      date: new Date('2020-10-12')
     },
     {
-      tittle: "tittle 01",
+      title: "tittle 01",
       content: "content 01",
       color: "red",
-      date: new Date.today
+      trash: false,
+      date: new Date('2020-10-12')
     },
     {
-      tittle: "tittle 01",
+      title: "tittle 01",
       content: "content 01",
       color: "red",
-      date: new Date.today
+      date: new Date('2020-10-12')
     },
   ],
 };
+
+function renderListItem(note) {
+  return `<li class="content__item">
+  <div class="content__detail">
+    <h3>${note.title}</h3>
+    <p>${note.content}</p>
+  </div>
+</li>`;
+}
+
+function renderList() {
+  GLOBAL.notes.sort((a, b) => b.createdDate - a.createdDate);
+  return `
+  <h2>Notes Global</h2>
+  <ul class="content__list">
+    ${GLOBAL.notes.map((note) => renderListItem(note)).join("")}
+  </ul>
+  <footer class="footer">
+    <button class="js-button footer__button">New Expense</button>
+  </footer>
+  `;
+}
+
+function addEventListeners() {
+}
+
+function init() {
+  const content = document.querySelector(".notes-container");
+  content.innerHTML = renderList();
+  addEventListeners();
+}
+init();
