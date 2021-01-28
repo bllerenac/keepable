ea298968ea0f1e304dad37320f4b38574df6ed2e
bllerenac <bllerenac@uni.pe>
2021-01-28 16:08:23 -0500

    add database in archive new_note.js

---
+++ new_note.js
@@ -0,0 +1,28 @@
+const global =  {
+  notes: [
+    {
+      tittle: "tittle 01",
+      content: "content 01",
+      color: "red",
+      date: new Date.today
+    },
+    {
+      tittle: "tittle 01",
+      content: "content 01",
+      color: "red",
+      date: new Date.today
+    },
+    {
+      tittle: "tittle 01",
+      content: "content 01",
+      color: "red",
+      date: new Date.today
+    },
+    {
+      tittle: "tittle 01",
+      content: "content 01",
+      color: "red",
+      date: new Date.today
+    },
+  ],
+};
